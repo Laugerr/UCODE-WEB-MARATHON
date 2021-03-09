2ff0d07428d98a7438c776a2fef7488e07b41f64
Salah <salaheddineelmanssourii@gmail.com>
2021-03-09 14:40:50 +0200

    Almost Done!

---
+++ Web-Sprint02/t03/js/script.js
@@ -0,0 +1,33 @@
+let chooseNumber = Number(prompt("Choose a number from 1 to 10"));
+
+
+
+switch (chooseNumber) {
+    case 1:
+        alert("Black to square 1");
+        break;
+    case 2:
+        alert("Goodu 2-shoes");
+        break;
+    case 3: 
+    case 6:
+        alert("Two's company, three's a crowd");
+        break;
+    case 4: 
+    case 9:
+        alert("Counting sheep");
+        break;
+    case 5:
+        alert("Take five");
+        break;
+    case 7:
+        alert("Seventh heaven");
+        break;
+    case 8:
+        alert("Behind the eight-ball");
+        break;
+    case 10:
+        alert("Cheaper by the dozen");
+    default:
+        prompt("Try again");
+}
